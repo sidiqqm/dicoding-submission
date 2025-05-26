3ca869a3bae40307b0b6572687d3d785f4f2095a
Muhammad Sidiq <moehammad.siddiq@gmail.com>
2025-05-26 20:43:22 +0700

    second deploy

--- webpack.common.js
+++ webpack.common.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'docs'),
     assetModuleFilename: 'assets/[hash][ext][query]',
     clean: true,
   },
